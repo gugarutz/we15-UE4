baf43919f87027a08ba41fb55160ea00424fcbdb
raimund <raimund.rittnauer@gmail.com>
2015-05-31 22:46:33 +0200

    Chapter 4 done

--- app/data/DBpediaDataInserter.java
+++ app/data/DBpediaDataInserter.java
@@ -26,14 +26,23 @@ public class DBpediaDataInserter {
     public static void fetchQuestions() {
         try {
             Category category = new Category();
-            category.setNameDE("Geologie");
-            category.setNameEN("Geology");
+            category.setNameDE("Film");
+            category.setNameEN("Movie");
 
-            category.addQuestion(createDBpediaQuestion("Was ist die Hauptstadt von Ungarn?", "What is the capital of Hungary?", 10, "Hungary"));
-            category.addQuestion(createDBpediaQuestion("Was ist die Hauptstadt von Paraguay?", "What is the capital of Paraguay?", 10, "Paraguay"));
-            category.addQuestion(createDBpediaQuestion("Was ist die Hauptstadt von Griechenland?", "What is the capital of Greece?", 10, "Greece"));
-            category.addQuestion(createDBpediaQuestion("Was ist die Hauptstadt von Libanon?", "What is the capital of Libanon?", 10, "Lebanon"));
-            category.addQuestion(createDBpediaQuestion("Was ist die Hauptstadt von Aserbaidschan?", "What is the capital of Azerbaijan?", 10, "Azerbaijan"));
+            category.addQuestion(createDBpediaQuestion("Welche Filme sind von Tim Burton?",
+                    "Tim Burton is the regisseur of which movies?", 30, "Tim_Burton"));
+
+            category.addQuestion(createDBpediaQuestion("Welche Filme sind von Quentin Tarantino?",
+                    "Quentin Tarantino is the regisseur of which movies?", 10, "Quentin_Tarantino"));
+
+            category.addQuestion(createDBpediaQuestion("Welche Filme sind von Jim Jarmusch?",
+                    "Jim Jarmusch is the regisseur of which movies?", 20, "Jim_Jarmusch"));
+
+            category.addQuestion(createDBpediaQuestion("Welche Filme sind von Robert Aldrich?",
+                    "Robert Aldrich is the regisseur of which movies?", 40, "Robert_Aldrich"));
+
+            category.addQuestion(createDBpediaQuestion("Welche Filme sind von Stanley Kramer?",
+                    "Stanley Kramer is the regisseur of which movies?", 60, "Stanley_Kramer"));
 
             JeopardyDAO.INSTANCE.persist(category);
         } catch (Exception ex) {
@@ -54,10 +63,10 @@ public class DBpediaDataInserter {
 
         // build SPARQL-query
         SelectQueryBuilder query = DBPediaService.createQueryBuilder()
-                .setLimit(5) // at most five statements
-                .addWhereClause(RDF.type, DBPediaOWL.capital)
+                .setLimit(2) // at most five statements
+                .addWhereClause(RDF.type, DBPediaOWL.Film)
                 .addPredicateExistsClause(FOAF.name)
-                .addWhereClause(DBPediaOWL.Place, validResource)
+                .addWhereClause(DBPediaOWL.director, validResource)
                 .addFilterClause(RDFS.label, Locale.GERMAN)
                 .addFilterClause(RDFS.label, Locale.ENGLISH);
 
@@ -66,28 +75,28 @@ public class DBpediaDataInserter {
         Model validModel = DBPediaService.loadStatements(query.toQueryString());
 
         // get english and german movie names, e.g., for right choices
-        List<String> englishValidCapitalName =
+        List<String> englishValidQuestionNames =
                 DBPediaService.getResourceNames(validModel, Locale.ENGLISH);
-        List<String> germanValidCapitalName =
+        List<String> germanValidQuestionNames =
                 DBPediaService.getResourceNames(validModel, Locale.GERMAN);
 
         // alter query to get movies without tim burton
-        query.removeWhereClause(DBPediaOWL.capitalCountry, validResource);
-        query.addMinusClause(DBPediaOWL.capitalCountry, validResource);
+        query.removeWhereClause(DBPediaOWL.director, validResource);
+        query.addMinusClause(DBPediaOWL.director, validResource);
 
         // retrieve data from dbpedia
         Model invalidModel = DBPediaService.loadStatements(query.toQueryString());
         // get english and german movie names, e.g., for wrong choices
-        List<String> englishInvalidCapitalName =
+        List<String> englishInvalidQuestionNames =
                 DBPediaService.getResourceNames(invalidModel, Locale.ENGLISH);
-        List<String> germanInvalidCapitalName =
+        List<String> germanInvalidQuestionNames =
                 DBPediaService.getResourceNames(invalidModel, Locale.GERMAN);
 
         // valid answers
-        addAnswers(question, englishValidCapitalName, germanValidCapitalName, true);
+        addAnswers(question, englishValidQuestionNames, germanValidQuestionNames, true);
 
         // invalid answers
-        addAnswers(question, englishInvalidCapitalName, germanInvalidCapitalName, false);
+        addAnswers(question, englishInvalidQuestionNames, germanInvalidQuestionNames, false);
 
         JeopardyDAO.INSTANCE.persist(question);
 
@@ -95,11 +104,14 @@ public class DBpediaDataInserter {
     }
 
     private static void addAnswers(Question question, List<String> englishAnswers, List<String> germanAnswers, boolean isCorrect) {
-        ArrayList<Answer> answers = new ArrayList<>();
 
         for(int i = 0; i < englishAnswers.size(); i++) {
             Answer answer = new Answer();
-            answer.setTextDE(germanAnswers.get(i));
+            if(germanAnswers.get(i).isEmpty())
+                answer.setTextDE(englishAnswers.get(i));
+            else
+                answer.setTextDE(germanAnswers.get(i));
+
             answer.setTextEN(englishAnswers.get(i));
 
             answer.setCorrectAnswer(isCorrect);
@@ -112,9 +124,7 @@ public class DBpediaDataInserter {
                 question.addWrongAnswer(answer);
             }
 
-            answers.add(answer);
+            question.getAnswers().add(answer);
         }
-
-        question.setAnswers(answers);
     }
 }
